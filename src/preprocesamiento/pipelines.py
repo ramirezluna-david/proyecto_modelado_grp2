f815fb2dd5b4f3f6f4e311782ca28b4fe38a4642
David Ramírez <davidrrluna@gmail.com>
2026-05-21 11:27:31 -0400

    commit

--- src/preprocesamiento/pipelines.py
+++ src/preprocesamiento/pipelines.py
@@ -1,3 +1,5 @@
+### Pipelines para cada tipo de conjunto
+
 # Define pipeline para variables numéricas
 pipeline_numerical_features = Pipeline(steps=[
     ('winsorizer', Winsorizer(limits=(0.05, 0.05))), # Aplica Winsorización para limitar outliers al 5%
@@ -16,3 +18,14 @@ pipeline_ordinales = Pipeline(steps=[
     ('imputer', SimpleImputer(strategy='most_frequent')), # Imputa valores faltantes con la moda
     ('ordinal', OrdinalEncoder(categories=[orden_tipo_plan, orden_uso_app])) # Aplica codificación OrdinalEncoder para variables ordinales con orden definido
 ])
+
+### Integración de pipelines de transformación
+# Combina pipelines para aplicar transformaciones específicas a cada tipo de variable
+preprocesador = ColumnTransformer(
+    transformers=[
+        ('num_limpios', pipeline_numerical_features, numerical_features),
+        ('cat_nom', pipeline_nominales, categorical_nominales),
+        ('cat_ord', pipeline_ordinales, categorical_ordinales),
+    ],
+    remainder='drop' # Elimina columnas no especificadas
+)
